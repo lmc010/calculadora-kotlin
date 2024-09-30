97964ee711d9716475ed2e7ee1b246acba3cdd39
Leonardo Mendonça <115121615+lmc010@users.noreply.github.com>
2024-09-30 18:35:19 -0300

    Adicionei os Arquivos

---
+++ CalculadoraKotlin/app/src/main/java/com/example/calculadorakotlin/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.calculadorakotlin
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.calculadorakotlin.databinding.ActivityMainBinding
+import com.example.calculadorakotlin.databinding.ActivityMainBinding.inflate
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
